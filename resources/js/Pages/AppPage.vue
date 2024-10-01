3b99b9189407edbfbc53854ba9758497e6f972a8
MD ANIKUL ISLAM <anikul.islam01@northsouth.edu>
2024-10-01 16:56:20 +0600

    first commit

---
+++ resources/js/Pages/AppPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+    <h1>App Page</h1>
+    <p>App Page content goes here</p>
+    <Link href="/home" class="nav-link active">
+        Click here to go to Home Page
+    </Link>
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/HomePage.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+    <h1>Home Page</h1>
+    <p>Home Page content goes here</p>
+    <Link href="/" class="nav-link active">
+        Click here to go to App Page
+    </Link>
+</div>
+</template>
+
+<style scoped>
+
+</style>
